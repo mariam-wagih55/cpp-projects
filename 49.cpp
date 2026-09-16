59a5da3c4ff4aaea7c12976763706135137f1f35
mariam-wagih55 <hjjagahkzjshbz@gmail.com>
2026-09-16 17:05:30 +0300

    Add files via upload

---
+++ 49.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int a[], int len) {
+    int k;
+
+    for (int i = 1; i < len; i++) {
+        k = a[i];
+        int j = i - 1;
+
+        while (j >= 0 && k < a[j]) {
+            a[j + 1] = a[j];
+            j--;
+        }
+
+        a[j + 1] = k;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int* a = new int[n];
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    insertionSort(a, n);
+
+    for (int i = 0; i < n; i++)
+        cout << a[i] << "   ";
+
+    delete[] a;
+
+    return 0;
+}
